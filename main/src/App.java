0780ef1b4e022a3391fe90a5c54a4d7bdbdd614a
Kohei Yoshikawa <marimo3418@neko2.net>
2021-04-15 09:15:42 +0000

    Wrote q3 function

--- main/src/App.java
+++ main/src/App.java
@@ -40,6 +40,22 @@ public class App {
     }
 
     static void q3(){
+        int percent, win = 0 , times;
 
+        Random rand = new Random();
+        System.out.println("くじの当選確率[%]はどうしますか？");
+        percent = input_int();
+        System.out.println("くじの試行回数[回]はどうしますか？");
+        times = input_int();
+
+        for(int i=0;i<times;i++){
+            if(percent>=rand.nextInt(100)){
+                win += 1;
+                System.out.println("当たりました。");
+            }
+            else System.out.println("はずれました。");
+        }
+
+        System.out.println("今回の試行における当選回数は、" + win + "回、確率は" + (float)win * 100 / times + "%でした。");
     }
 }
